ea56aaf7c6f49e046b419f52c77ca51966650a2d
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-02-16 20:24:51 +0000

    Add both lights

--- audio.js
+++ audio.js
@@ -1,6 +1,6 @@
 const DMX = require('./api');
 
-DMX.setLength(10);
+DMX.setLength(15);
 
 DMX.setColor(1, 2, 255, 255, 255);
 DMX.setColor(1, 9, 255, 255, 255);
@@ -31,11 +31,16 @@ DMX.setColor(500, 9, 255, 255, 255);
 
 DMX.onOff(500, 75, 1); // Your love
 DMX.onOff(600, 75, 1); // Your love
+DMX.onOff(500, 75, 8); // Your love
+DMX.onOff(600, 75, 8); // Your love
 
 DMX.onOff(1500, 100, 1); // Ping
+DMX.onOff(1500, 100, 8); // Ping
 
 DMX.onOff(2500, 75, 1); // Your love
 DMX.onOff(2600, 75, 1); // Your love
+DMX.onOff(2500, 75, 8); // Your love
+DMX.onOff(2600, 75, 8); // Your love
 
 DMX.onOff(3400, 100, 1);
 DMX.onOff(3400, 100, 8);
@@ -43,9 +48,11 @@ DMX.onOff(3400, 100, 8);
 DMX.add(4400, 50, 1, 0, 255);
 DMX.add(4400, 50, 8, 0, 255);
 
+DMX.setColorFade(5500, 2, 100, 255, 0, 0);
+DMX.setColorFade(5500, 9, 100, 0, 0, 255);
 
-DMX.setColorFade(6500, 2, 100, 255, 0, 0);
-DMX.setColorFade(6500, 9, 100, 0, 0, 255);
+DMX.setColorFade(6500, 2, 100, 0, 0, 255);
+DMX.setColorFade(6500, 9, 100, 255, 0, 0);
 
 
 DMX.export();
