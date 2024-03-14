3b57714f47fb9a41b1679fa5a481078fc63022ff
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-03-14 19:49:49 +0000

    E

--- index.js
+++ index.js
@@ -18,9 +18,31 @@ async function main() {
         4: 235
     });
 
-    setInterval(() => {
-        blink(uni, 1, 500);
-    }, 1000);
+    const ArtNet = require('artnode');
+ 
+    const artnet = new Artnet({isController: true});
+    
+    const universe = artnet.getUniverse(0);
+    
+    universe.on('data', ({ data, changed }) => {
+        changed.forEach(({ address, value }) => {
+            console.log(`DMX ${address} set to ${value}.`);
+
+            var toUp;
+            toUp[address] = value;
+            uni.update(toUp);
+        });
+        
+        data.forEach((value, address) => {
+            console.log(`DMX ${address} is ${value}`);
+        });
+    });
+    
+    artnet.start();
+
+    // setInterval(() => {
+    //     blink(uni, 1, 500);
+    // }, 1000);
 }
 
 function blink(uni, light, dur) {
